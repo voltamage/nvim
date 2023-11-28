e18cb47c990479247fcdb1175493553ae6a8faae
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-27 22:23:09 -0800

    add lualine and lazy conf

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -11,4 +11,31 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('main.plugins')
+require('lazy').setup({
+  {import = 'main.plugins'},
+  {import = 'main.plugins.lsp'}
+},
+{
+  defaults = {
+    version = '*'
+  },
+  install = {
+    colorscheme = { 'gruvbox-material' }
+  },
+  diff = {
+    -- diff command <d> can be one of:
+    -- * browser: opens the github compare view. Note that this is always mapped to <K> as well,
+    --   so you can have a different command for diff <d>
+    -- * git: will run git diff and open a buffer with filetype git
+    -- * terminal_git: will open a pseudo terminal with git diff
+    -- * diffview.nvim: will open Diffview to show the diff
+    cmd = "git"
+  },
+  checker = {
+    enabled = true,
+    notify = false
+  },
+  change_detection = {
+    notify = false
+  },
+})

22e25fef32a29388ca7984c9f97a80281ba37c31
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-28 19:58:54 -0800

    lazy load colorizer

--- lua/main/plugins/colorizer.lua
+++ lua/main/plugins/colorizer.lua
@@ -1,5 +1,9 @@
 return {
   'NvChad/nvim-colorizer.lua',
+  event = {
+    'BufReadPre',
+    'BufNewFile',
+  },
   config = function()
     require('colorizer').setup({
       filetypes = { '*' },

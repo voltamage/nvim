c5957b3b3e307629c6a5ea841dd66fc0d9bb24b9
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-28 19:12:58 -0800

    added neorg and updated old file

--- old.lua
+++ old.lua
@@ -1,119 +1,88 @@
---vim.g.loaded_netrw = 1        -- NOTE: this might help with neo-tree
---vim.g.loaded_netrwPlugin = 1  -- NOTE: this might help with neo-tree
-
---vim.o.cmdheight = 2 -- NOTE: allows hardtime to display alongside modes
+vim.g.loaded_netrw = 1 -- NOTE: this might help with neo-tree
+vim.g.loaded_netrwPlugin = 1 -- NOTE: this might help with neo-tree
+vim.o.cmdheight = 2 -- NOTE: allows hardtime to display alongside modes
 vim.o.termguicolors = true -- NOTE: relied on by neo-tree
 
-require('lazy').setup({
-  -- {
-  --   'NvChad/nvim-colorizer.lua',
-  --   config = function()
-  --     require('colorizer').setup(nil,{
-  --       RGB       = true,
-  --       RRGGBB    = true,
-  --       RRGGBBAA  = true,
-  --       hsl_fn    = true,
-  --       mode      = 'background',
-  --       names     = false,
-  --       rgb_fn    = true,
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   'nvim-neo-tree/neo-tree.nvim',
-  --   branch = 'v3.x',
-  --   dependencies = {
-  --     'nvim-tree/nvim-web-devicons',
-  --     'nvim-lua/plenary.nvim',
-  --     'MunifTanjim/nui.nvim',
-  --   },
-  --   config = true,
-  -- },
-  -- {
-  --   'folke/trouble.nvim',
-  --   dependencies = {
-  --     'nvim-tree/nvim-web-devicons'
-  --   },
-  --   opts = {},
-  -- },
-  {
-    'nvim-neorg/neorg',
-    build = ':Neorg sync-parsers', -- BUG: does not seem to trigger properly, complains on first run
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      require('neorg').setup({
-        load = {
-          ['core.defaults'] = {}, -- Loads default behaviour
-          ['core.concealer'] = {}, -- Adds pretty icons to your documents
-          ['core.dirman'] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                dotfiles = '~/dotfiles/org',
-              },
-              default_workspace = 'dotfiles',
-            },
-          },
-        },
-      })
-      vim.wo.conceallevel = 2 -- TODO: difference between vim.o and vim.wo
-    end,
-  },
-  --{
-  --  'ellisonleao/glow.nvim',
-  --  config = true,
-  --  cmd = 'Glow'
-  --},
-  -- {
-  --   'folke/twilight.nvim', -- BUG: does nothing
-  --   config = true,
-  -- },
-  -- {
-  --   'lcheylus/overlength.nvim',
-  --   config = true,
-  -- },
-  -- {
-  --   'xiyaowong/transparent.nvim',
-  --   config = function()
-  --     require('transparent').setup {
-  --       extra_groups = {
-  --         'NeoTreeNormal',
-  --         'NeoTreeNormalNC',
-  --         'NormalFloat',
-  --       },
-  --    }
-  --   end,
-  -- },
-  -- {
-  --   "m4xshen/hardtime.nvim",
-  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  --   opts = {
-  --     enabled = false,
-  --   }
-  -- },
-  -- {
-  -- 'nvim-pack/nvim-spectre',
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim'
-  --   },
-  -- config = function()
-  --   require('spectre').setup({
-  --     live_update = true,
-  --     replace_vim_cmd = 'cdfo',
-  --     })
-  --   end,
-  -- },
-  -- {
-  -- 'akinsho/toggleterm.nvim',
-  -- config = function()
-  --   require('toggleterm').setup({
-  --     shade_terminals = false,
-  --     })
-  --   end,
-  -- },
-  defaults = {
-    lazy = true,
-  },
-})
+-- {
+--   'NvChad/nvim-colorizer.lua',
+--   config = function()
+--     require('colorizer').setup(nil,{
+--       RGB       = true,
+--       RRGGBB    = true,
+--       RRGGBBAA  = true,
+--       hsl_fn    = true,
+--       mode      = 'background',
+--       names     = false,
+--       rgb_fn    = true,
+--     })
+--   end,
+-- },
+-- {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   branch = 'v3.x',
+--   dependencies = {
+--     'nvim-tree/nvim-web-devicons',
+--     'nvim-lua/plenary.nvim',
+--     'MunifTanjim/nui.nvim',
+--   },
+--   config = true,
+-- },
+-- {
+--   'folke/trouble.nvim',
+--   dependencies = {
+--     'nvim-tree/nvim-web-devicons'
+--   },
+--   opts = {},
+-- },
+--{
+--  'ellisonleao/glow.nvim',
+--  config = true,
+--  cmd = 'Glow'
+--},
+-- {
+--   'folke/twilight.nvim', -- BUG: does nothing
+--   config = true,
+-- },
+-- {
+--   'lcheylus/overlength.nvim',
+--   config = true,
+-- },
+-- {
+--   'xiyaowong/transparent.nvim',
+--   config = function()
+--     require('transparent').setup {
+--       extra_groups = {
+--         'NeoTreeNormal',
+--         'NeoTreeNormalNC',
+--         'NormalFloat',
+--       },
+--    }
+--   end,
+-- },
+-- {
+--   "m4xshen/hardtime.nvim",
+--   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+--   opts = {
+--     enabled = false,
+--   }
+-- },
+-- {
+-- 'nvim-pack/nvim-spectre',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim'
+--   },
+-- config = function()
+--   require('spectre').setup({
+--     live_update = true,
+--     replace_vim_cmd = 'cdfo',
+--     })
+--   end,
+-- },
+-- {
+-- 'akinsho/toggleterm.nvim',
+-- config = function()
+--   require('toggleterm').setup({
+--     shade_terminals = false,
+--     })
+--   end,
+-- },

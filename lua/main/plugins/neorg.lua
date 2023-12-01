ae6e42acd9fd287e5e0018d680ef4c1c22fe8fb6
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-30 16:57:39 -0800

    added leap and updated neorg parse

--- lua/main/plugins/neorg.lua
+++ lua/main/plugins/neorg.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-neorg/neorg',
-  build = ':Neorg sync-parsers',
+  run = ':Neorg sync-parsers',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',

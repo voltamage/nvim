6512557b64587fdddbb0eae4a21bceb434c210e7
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-02 15:48:07 -0800

    changed start page theme

--- lua/main/plugins/alpha.lua
+++ lua/main/plugins/alpha.lua
@@ -1,6 +1,6 @@
 return {
   'goolord/alpha-nvim',
   config = function()
-    require('alpha').setup(require('alpha.themes.dashboard').config)
+    require('alpha').setup(require('alpha.themes.startify').config)
   end,
 }

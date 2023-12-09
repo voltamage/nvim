9bbb09887e3f8630178fa1c172701d292ee3759f
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-09 13:10:09 -0800

    added completion stuff

--- lua/main/plugins/lsp/cmp.lua
+++ lua/main/plugins/lsp/cmp.lua
@@ -11,4 +11,40 @@ return {
     'rafamadriz/friendly-snippets',
     'saadparwaiz1/cmp_luasnip',
   },
+  config = function()
+    local cmp = require('cmp')
+    local lspkind = require('lspkind')
+    local luasnip = require('luasnip')
+    require('luasnip.loaders.from_vscode').lazy_load()
+    cmp.setup({
+      completion = {
+        completeopt = 'menu,menuone,preview,noselect',
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function(entry, vim_item)
+            return vim_item
+          end,
+        }),
+      },
+    })
+  end,
 }

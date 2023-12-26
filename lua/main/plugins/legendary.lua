e65a77f9ff49b18fab1e492eddec28d7517bc17a
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-26 06:56:03 -0800

    added legendary instead of cheatsheet

---
+++ lua/main/plugins/legendary.lua
@@ -0,0 +1,10 @@
+return {
+  'mrjones2014/legendary.nvim',
+  dependencies = {
+    'kkharji/sqlite.lua',
+    'nvim-telescope/telescope.nvim',
+    'stevearc/dressing.nvim',
+  },
+  priority = 1001,
+  lazy = false,
+}
